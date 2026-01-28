27d5a85beb62dc85e8a313c5554dae39418145f4
OptiCAPI <webtracker61@gmail.com>
2026-01-29 04:05:14 +0600

    Create term1.js

---
+++ results/2025/1/term1.js
@@ -0,0 +1,17 @@
+const data_2025_1_term1 = [
+    {
+        roll: "1",
+        name: "আহসান হাবিব",
+        marks: [
+            { subject: "বাংলা", obtained: 88 },
+            { subject: "গণিত", obtained: 92 },
+            { subject: "ইংরেজি", obtained: 85 },
+            { subject: "বাংলাদেশ ও বিশ্বপরিচয়", obtained: 80 },
+            { subject: "সাধারণ বিজ্ঞান", obtained: 87 },
+            { subject: "ধর্ম ও নৈতিক শিক্ষা", obtained: 95 },
+            { subject: "শিল্পকলা", obtained: 82 },
+            { subject: "শা. ও মানসিক স্বাস্থ্য শিক্ষা", obtained: 90 },
+            { subject: "সঙ্গীত", obtained: 84 }
+        ]
+    }
+];
