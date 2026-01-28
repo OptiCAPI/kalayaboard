fdec794105e6b5fe7b0b633a5e38244efd74858f
OptiCAPI <webtracker61@gmail.com>
2026-01-29 01:57:36 +0600

    Add student database for class 5
    Added student data for class 5, including names, roll numbers, and marks for various subjects.

---
+++ data_2025_class5.js
@@ -0,0 +1,31 @@
+// কালাইয়া বোর্ড সপ্রাবি - শিক্ষার্থী ডাটাবেজ
+const studentData = [
+    {
+        roll: "1",
+        name: "মোঃ আব্দুল্লাহ",
+        class: "৫ম শ্রেণি",
+        term: "২য় প্রান্তিক মূল্যায়ন",
+        marks: [
+            { subject: "বাংলা", full: 100, obtained: 85 },
+            { subject: "গণিত", full: 100, obtained: 92 },
+            { subject: "ইংরেজি", full: 100, obtained: 78 },
+            { subject: "বাওবি", full: 100, obtained: 88 },
+            { subject: "প্রাথমিক বিজ্ঞান", full: 100, obtained: 80 },
+            { subject: "ধর্ম ও নৈতিক শিক্ষা", full: 100, obtained: 95 }
+        ]
+    },
+    {
+        roll: "2",
+        name: "মোসাম্মাৎ ফাতেমা",
+        class: "৫ম শ্রেণি",
+        term: "২য় প্রান্তিক মূল্যায়ন",
+        marks: [
+            { subject: "বাংলা", full: 100, obtained: 75 },
+            { subject: "গণিত", full: 100, obtained: 65 },
+            { subject: "ইংরেজি", full: 100, obtained: 70 },
+            { subject: "বাওবি", full: 100, obtained: 72 },
+            { subject: "প্রাথমিক বিজ্ঞান", full: 100, obtained: 68 },
+            { subject: "ধর্ম ও নৈতিক শিক্ষা", full: 100, obtained: 80 }
+        ]
+    }
+];
