c7ed3047c5437e3ede8889ed825680fef367feb7
OptiCAPI <webtracker61@gmail.com>
2026-01-29 20:41:51 +0600

    Update term3.js

--- results/2024/5/term3.js
+++ results/2024/5/term3.js
@@ -3,42 +3,82 @@
 
 const data_2024_5_term3 = [
     {
-        roll: "01",
-        name: "শানজিদা আক্তার (Sanjida Akter)",
+        roll: "01", name: "Shanjiida Akter",
         marks: [
-            { subject: "বাংলা", obtained: 88 },
-            { subject: "ইংরেজি", obtained: 97 },
-            { subject: "প্রাথমিক গণিত", obtained: 93 },
-            { subject: "বাংলাদেশ ও বিশ্ব পরিচয়", obtained: 99 },
-            { subject: "প্রাথমিক বিজ্ঞান", obtained: 94 },
-            { subject: "ধর্ম ও নৈতিক শিক্ষা", obtained: 98 },
-            { subject: "শারীরিক-মানসিক স্বাস্থ্য", obtained: 46 }, // আপনার শিট অনুযায়ী
-            { subject: "সঙ্গীত ও শিল্পকলা", obtained: 47 }
+            { subject: "বাংলা", obtained: 88 }, { subject: "ইংরেজি", obtained: 97 },
+            { subject: "প্রাথমিক গণিত", obtained: 93 }, { subject: "বাংলাদেশ ও বিশ্ব পরিচয়", obtained: 99 },
+            { subject: "প্রাথমিক বিজ্ঞান", obtained: 94 }, { subject: "ধর্ম ও নৈতিক শিক্ষা", obtained: 98 },
+            { subject: "শারীরিক-মানসিক স্বাস্থ্য", obtained: 46 }, { subject: "সঙ্গীত ও শিল্পকলা", obtained: 47 }
         ]
     },
     {
-        roll: "02",
-        name: "মারজিয়া তুনা (Marzia Tuna)",
+        roll: "02", name: "Marzia Tuna",
         marks: [
-            { subject: "বাংলা", obtained: 77 },
-            { subject: "ইংরেজি", obtained: 83 },
-            { subject: "প্রাথমিক গণিত", obtained: 83 },
-            { subject: "বাংলাদেশ ও বিশ্ব পরিচয়", obtained: 90 },
-            { subject: "প্রাথমিক বিজ্ঞান", obtained: 85 },
-            { subject: "ধর্ম ও নৈতিক শিক্ষা", obtained: 90 },
-            { subject: "শারীরিক-মানসিক স্বাস্থ্য", obtained: 44 },
-            { subject: "সঙ্গীত ও শিল্পকলা", obtained: 42 }
+            { subject: "বাংলা", obtained: 77 }, { subject: "ইংরেজি", obtained: 83 },
+            { subject: "প্রাথমিক গণিত", obtained: 83 }, { subject: "বাংলাদেশ ও বিশ্ব পরিচয়", obtained: 90 },
+            { subject: "প্রাথমিক বিজ্ঞান", obtained: 85 }, { subject: "ধর্ম ও নৈতিক শিক্ষা", obtained: 90 },
+            { subject: "শারীরিক-মানসিক স্বাস্থ্য", obtained: 44 }, { subject: "সঙ্গীত ও শিল্পকলা", obtained: 42 }
         ]
     },
     // রোল ০৩ থেকে ১৮ পর্যন্ত ডাটা এক্সেল শিটে খালি (০) থাকায় নিচে শুধু রোল ও নাম দেওয়া হলো
-    { roll: "03", name: "ফাতেমা (Fatima)", marks: [] },
-    { roll: "04", name: "মারজান (Marjan)", marks: [] },
-    { roll: "05", name: "শাকিল (Shakil)", marks: [] },
-    { roll: "06", name: "তামিম (Tamim)", marks: [] },
-    { roll: "07", name: "রিয়াজ (Riyaj)", marks: [] },
-    { roll: "08", name: "আরিয়ান (Ariyan)", marks: [] },
-    { roll: "09", name: "লিমন (Limon)", marks: [] },
-    { roll: "10", name: "নাহিন (Nahin)", marks: [] },
+  
+     { roll: "03", name: "ফাতেমা (Fatima)", 
+      marks: [
+            { subject: "বাংলা", obtained: 0 }, { subject: "ইংরেজি", obtained: 0 },
+            { subject: "প্রাথমিক গণিত", obtained: 0 }, { subject: "বাংলাদেশ ও বিশ্ব পরিচয়", obtained: 0 },
+            { subject: "প্রাথমিক বিজ্ঞান", obtained: 0 }, { subject: "ধর্ম ও নৈতিক শিক্ষা", obtained: 0 },
+            { subject: "শারীরিক-মানসিক স্বাস্থ্য", obtained: 0 }, { subject: "সঙ্গীত ও শিল্পকলা", obtained: 0 }
+        ]
+    },
+    { roll: "04", name: "মারজান (Marjan)",
+     marks: [{ subject: "বাংলা", obtained: 0 }, { subject: "ইংরেজি", obtained: 0 },
+            { subject: "প্রাথমিক গণিত", obtained: 0 }, { subject: "বাংলাদেশ ও বিশ্ব পরিচয়", obtained: 0 },
+            { subject: "প্রাথমিক বিজ্ঞান", obtained: 0 }, { subject: "ধর্ম ও নৈতিক শিক্ষা", obtained: 0 },
+            { subject: "শারীরিক-মানসিক স্বাস্থ্য", obtained: 0 }, { subject: "সঙ্গীত ও শিল্পকলা", obtained: 0 }
+       ] 
+    },
+    { roll: "05", name: "শাকিল (Shakil)", 
+     marks: [{ subject: "বাংলা", obtained: 0 }, { subject: "ইংরেজি", obtained: 0 },
+            { subject: "প্রাথমিক গণিত", obtained: 0 }, { subject: "বাংলাদেশ ও বিশ্ব পরিচয়", obtained: 0 },
+            { subject: "প্রাথমিক বিজ্ঞান", obtained: 0 }, { subject: "ধর্ম ও নৈতিক শিক্ষা", obtained: 0 },
+            { subject: "শারীরিক-মানসিক স্বাস্থ্য", obtained: 0 }, { subject: "সঙ্গীত ও শিল্পকলা", obtained: 0 }
+       ] 
+    },
+    { roll: "06", name: "তামিম (Tamim)", 
+     marks: [{ subject: "বাংলা", obtained: 0 }, { subject: "ইংরেজি", obtained: 0 },
+            { subject: "প্রাথমিক গণিত", obtained: 0 }, { subject: "বাংলাদেশ ও বিশ্ব পরিচয়", obtained: 0 },
+            { subject: "প্রাথমিক বিজ্ঞান", obtained: 0 }, { subject: "ধর্ম ও নৈতিক শিক্ষা", obtained: 0 },
+            { subject: "শারীরিক-মানসিক স্বাস্থ্য", obtained: 0 }, { subject: "সঙ্গীত ও শিল্পকলা", obtained: 0 }
+       ] 
+    },
+    { roll: "07", name: "রিয়াজ (Riyaj)", 
+     marks: [{ subject: "বাংলা", obtained: 0 }, { subject: "ইংরেজি", obtained: 0 },
+            { subject: "প্রাথমিক গণিত", obtained: 0 }, { subject: "বাংলাদেশ ও বিশ্ব পরিচয়", obtained: 0 },
+            { subject: "প্রাথমিক বিজ্ঞান", obtained: 0 }, { subject: "ধর্ম ও নৈতিক শিক্ষা", obtained: 0 },
+            { subject: "শারীরিক-মানসিক স্বাস্থ্য", obtained: 0 }, { subject: "সঙ্গীত ও শিল্পকলা", obtained: 0 }
+       ]
+    },
+    { roll: "08", name: "আরিয়ান (Ariyan)", 
+     marks: [{ subject: "বাংলা", obtained: 0 }, { subject: "ইংরেজি", obtained: 0 },
+            { subject: "প্রাথমিক গণিত", obtained: 0 }, { subject: "বাংলাদেশ ও বিশ্ব পরিচয়", obtained: 0 },
+            { subject: "প্রাথমিক বিজ্ঞান", obtained: 0 }, { subject: "ধর্ম ও নৈতিক শিক্ষা", obtained: 0 },
+            { subject: "শারীরিক-মানসিক স্বাস্থ্য", obtained: 0 }, { subject: "সঙ্গীত ও শিল্পকলা", obtained: 0 }
+       ] 
+    },
+    { roll: "09", name: "লিমন (Limon)", 
+     marks: [{ subject: "বাংলা", obtained: 0 }, { subject: "ইংরেজি", obtained: 0 },
+            { subject: "প্রাথমিক গণিত", obtained: 0 }, { subject: "বাংলাদেশ ও বিশ্ব পরিচয়", obtained: 0 },
+            { subject: "প্রাথমিক বিজ্ঞান", obtained: 0 }, { subject: "ধর্ম ও নৈতিক শিক্ষা", obtained: 0 },
+            { subject: "শারীরিক-মানসিক স্বাস্থ্য", obtained: 0 }, { subject: "সঙ্গীত ও শিল্পকলা", obtained: 0 }
+       ] 
+    },
+    { roll: "10", name: "নাহিন (Nahin)", 
+     marks: [{ subject: "বাংলা", obtained: 0 }, { subject: "ইংরেজি", obtained: 0 },
+            { subject: "প্রাথমিক গণিত", obtained: 0 }, { subject: "বাংলাদেশ ও বিশ্ব পরিচয়", obtained: 0 },
+            { subject: "প্রাথমিক বিজ্ঞান", obtained: 0 }, { subject: "ধর্ম ও নৈতিক শিক্ষা", obtained: 0 },
+            { subject: "শারীরিক-মানসিক স্বাস্থ্য", obtained: 0 }, { subject: "সঙ্গীত ও শিল্পকলা", obtained: 0 }
+       ] 
+    },
     { roll: "11", name: "জিম (Jim)", marks: [] },
     { roll: "12", name: "মুসকান (Muskan)", marks: [] },
     { roll: "13", name: "হাফিজা (Hafija)", marks: [] },
