6825385bcb195d1955674d0d7b7e34f41f4a78e2
OptiCAPI <webtracker61@gmail.com>
2026-01-29 08:39:53 +0600

    Update term3.js

--- results/2025/5/term3.js
+++ results/2025/5/term3.js
@@ -1,69 +1,34 @@
 // কালাইয়া বোর্ড সপ্রাবি - শিক্ষার্থী ডাটাবেজ
-const data_2026_5_term2 = [
+const data_2025_5_term3 = [
     {
-        roll: "5",
-        name: "মোঃ আরিফ হোসেন",
+        roll: "25",
+        name: "তামিম ইকবাল",
         marks: [
-            { subject: "বাংলা", obtained: 85 },
-            { subject: "গণিত", obtained: 72 },
-            { subject: "ইংরেজি", obtained: 65 },
-            { subject: "বাংলাদেশ ও বিশ্বপরিচয়", obtained: 69 },
-            { subject: "সাধারণ বিজ্ঞান", obtained: 88 },
-            { subject: "ধর্ম ও নৈতিক শিক্ষা", obtained: 95 },
-            { subject: "শিল্পকলা", obtained: 80 },
-            { subject: "শা. ও মানসিক স্বাস্থ্য শিক্ষা", obtained: 82 },
-            { subject: "সঙ্গীত", obtained: 78 }
-        ]
-    }
-    // Add more students here...
-];
-
-
-
-
-// Version 2
-const studentData = [
-    {
-        roll: "1",
-        name: "মোঃ আব্দুল্লাহ",
-        class: "৫ম শ্রেণি",
-        term: "২য় প্রান্তিক মূল্যায়ন",
-        marks: [
-            { subject: "বাংলা", full: 100, obtained: 85 },
-            { subject: "গণিত", full: 100, obtained: 92 },
-            { subject: "ইংরেজি", full: 100, obtained: 78 },
-            { subject: "বাওবি", full: 100, obtained: 88 },
-            { subject: "প্রাথমিক বিজ্ঞান", full: 100, obtained: 80 },
-            { subject: "ধর্ম ও নৈতিক শিক্ষা", full: 100, obtained: 95 }
+            { subject: "বাংলা", obtained: 65 },
+            { subject: "গণিত", obtained: 58 },
+            { subject: "ইংরেজি", obtained: 62 },
+            { subject: "বাংলাদেশ ও বিশ্বপরিচয়", obtained: 60 },
+            { subject: "সাধারণ বিজ্ঞান", obtained: 68 },
+            { subject: "ধর্ম ও নৈতিক শিক্ষা", obtained: 75 },
+            { subject: "শিল্পকলা", obtained: 70 },
+            { subject: "শা. ও মানসিক স্বাস্থ্য শিক্ষা", obtained: 72 },
+            { subject: "সঙ্গীত", obtained: 66 }
         ]
     },
     {
-        roll: "2",
-        name: "মোসাম্মাৎ ফাতেমা",
-        class: "৫ম শ্রেণি",
-        term: "২য় প্রান্তিক মূল্যায়ন",
+        roll: "25",
+        name: "তামিম ইকবাল",
         marks: [
-            { subject: "বাংলা", full: 100, obtained: 75 },
-            { subject: "গণিত", full: 100, obtained: 65 },
-            { subject: "ইংরেজি", full: 100, obtained: 70 },
-            { subject: "বাওবি", full: 100, obtained: 72 },
-            { subject: "প্রাথমিক বিজ্ঞান", full: 100, obtained: 68 },
-            { subject: "ধর্ম ও নৈতিক শিক্ষা", full: 100, obtained: 80 }
+            { subject: "বাংলা", obtained: 65 },
+            { subject: "গণিত", obtained: 58 },
+            { subject: "ইংরেজি", obtained: 62 },
+            { subject: "বাংলাদেশ ও বিশ্বপরিচয়", obtained: 60 },
+            { subject: "সাধারণ বিজ্ঞান", obtained: 68 },
+            { subject: "ধর্ম ও নৈতিক শিক্ষা", obtained: 75 },
+            { subject: "শিল্পকলা", obtained: 70 },
+            { subject: "শা. ও মানসিক স্বাস্থ্য শিক্ষা", obtained: 72 },
+            { subject: "সঙ্গীত", obtained: 66 }
         ]
     },
-    {
-        roll: "5",
-        name: "মোঃ আরিফ হোসেন",
-        class: "৫ম শ্রেণি",
-        term: "২য় প্রান্তিক মূল্যায়ন",
-        marks: [
-            { subject: "বাংলা", full: 100, obtained: 85 },
-            { subject: "গণিত", full: 100, obtained: 72 },
-            { subject: "ইংরেজি", full: 100, obtained: 65 },
-            { subject: "বাওবি", full: 100, obtained: 69 },
-            { subject: "প্রাথমিক বিজ্ঞান", full: 100, obtained: 88 },
-            { subject: "ধর্ম ও নৈতিক শিক্ষা", full: 100, obtained: 95 }
-        ]
-    }
-    // Add more students here...
+    // Add more here...
 ];
