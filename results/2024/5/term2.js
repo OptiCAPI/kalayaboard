47d2a13d99b718016e576ec25e386b32a5e33f90
OptiCAPI <webtracker61@gmail.com>
2026-01-29 20:31:55 +0600

    Update term2.js

--- results/2024/5/term2.js
+++ results/2024/5/term2.js
@@ -3,31 +3,21 @@
 
 const data_2024_5_term2 = [
     {
-        roll: "01",
-        name: "শানজিদা আক্তার (Sanjida Akter)",
+        roll: "01", name: "Shanjiida Akter",
         marks: [
-            { subject: "বাংলা", obtained: 95 },
-            { subject: "ইংরেজি", obtained: 76 },
-            { subject: "প্রাথমিক গণিত", obtained: 64 },
-            { subject: "বাংলাদেশ ও বিশ্ব পরিচয়", obtained: 97 },
-            { subject: "প্রাথমিক বিজ্ঞান", obtained: 93 },
-            { subject: "ধর্ম ও নৈতিক শিক্ষা", obtained: 92 },
-            { subject: "শারীরিক-মানসিক স্বাস্থ্য", obtained: 45 },
-            { subject: "সঙ্গীত ও শিল্পকলা", obtained: 46 }
+            { subject: "বাংলা", obtained: 95 }, { subject: "ইংরেজি", obtained: 76 },
+            { subject: "প্রাথমিক গণিত", obtained: 64 }, { subject: "বাংলাদেশ ও বিশ্ব পরিচয়", obtained: 97 },
+            { subject: "প্রাথমিক বিজ্ঞান", obtained: 93 }, { subject: "ধর্ম ও নৈতিক শিক্ষা", obtained: 92 },
+            { subject: "শারীরিক-মানসিক স্বাস্থ্য", obtained: 45 }, { subject: "সঙ্গীত ও শিল্পকলা", obtained: 46 }
         ]
     },
     {
-        roll: "02",
-        name: "মারজিয়া তুনা (Marzia Tuna)",
+        roll: "02", name: "Marzia Tuna",
         marks: [
-            { subject: "বাংলা", obtained: 80 },
-            { subject: "ইংরেজি", obtained: 58 },
-            { subject: "প্রাথমিক গণিত", obtained: 45 },
-            { subject: "বাংলাদেশ ও বিশ্ব পরিচয়", obtained: 80 },
-            { subject: "প্রাথমিক বিজ্ঞান", obtained: 59 },
-            { subject: "ধর্ম ও নৈতিক শিক্ষা", obtained: 82 },
-            { subject: "শারীরিক-মানসিক স্বাস্থ্য", obtained: 42 },
-            { subject: "সঙ্গীত ও শিল্পকলা", obtained: 40 }
+            { subject: "বাংলা", obtained: 80 }, { subject: "ইংরেজি", obtained: 58 },
+            { subject: "প্রাথমিক গণিত", obtained: 45 }, { subject: "বাংলাদেশ ও বিশ্ব পরিচয়", obtained: 80 },
+            { subject: "প্রাথমিক বিজ্ঞান", obtained: 59 }, { subject: "ধর্ম ও নৈতিক শিক্ষা", obtained: 82 },
+            { subject: "শারীরিক-মানসিক স্বাস্থ্য", obtained: 42 }, { subject: "সঙ্গীত ও শিল্পকলা", obtained: 40 }
         ]
     }
 ];
