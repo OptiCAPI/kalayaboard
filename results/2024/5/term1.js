c6562b93a3315a876cbe8042bd2df75da4b706af
OptiCAPI <webtracker61@gmail.com>
2026-01-29 20:30:40 +0600

    Update term1.js

--- results/2024/5/term1.js
+++ results/2024/5/term1.js
@@ -1,31 +1,20 @@
-const data_2023_5_term1 = [
+const data_2024_5_term1 = [
     {
-        roll: "01",
-        name: "মোসাঃ শানজিদা আক্তার (MST SANJIDA AKTER)",
+        roll: "01", name: "Shanjiida Akter",
         marks: [
-            { subject: "বাংলা", obtained: 91 },
-            { subject: "ইংরেজি", obtained: 90 },
-            { subject: "প্রাথমিক গণিত", obtained: 54 },
-            { subject: "বাংলাদেশ ও বিশ্ব পরিচয়", obtained: 80 },
-            { subject: "প্রাথমিক বিজ্ঞান", obtained: 82 },
-            { subject: "ধর্ম ও নৈতিক শিক্ষা", obtained: 92 },
-            { subject: "শারীরিক-মানসিক স্বাস্থ্য", obtained: 45 },
-            { subject: "সঙ্গীত ও শিল্পকলা", obtained: 42 }
+            { subject: "বাংলা", obtained: 91 }, { subject: "ইংরেজি", obtained: 90 },
+            { subject: "প্রাথমিক গণিত", obtained: 54 }, { subject: "বাংলাদেশ ও বিশ্ব পরিচয়", obtained: 80 },
+            { subject: "প্রাথমিক বিজ্ঞান", obtained: 82 }, { subject: "ধর্ম ও নৈতিক শিক্ষা", obtained: 92 },
+            { subject: "শারীরিক-মানসিক স্বাস্থ্য", obtained: 45 }, { subject: "সঙ্গীত ও শিল্পকলা", obtained: 42 }
         ]
     },
     {
-        roll: "02",
-        name: "মারজিয়া তুনা (Marzia Tuna)",
+        roll: "02", name: "Marzia Tuna",
         marks: [
-            { subject: "বাংলা", obtained: 64 },
-            { subject: "ইংরেজি", obtained: 73 },
-            { subject: "প্রাথমিক গণিত", obtained: 33 },
-            { subject: "বাংলাদেশ ও বিশ্ব পরিচয়", obtained: 52 },
-            { subject: "প্রাথমিক বিজ্ঞান", obtained: 51 },
-            { subject: "ধর্ম ও নৈতিক শিক্ষা", obtained: 76 },
-            { subject: "শারীরিক-মানসিক স্বাস্থ্য", obtained: 48 },
-            { subject: "সঙ্গীত ও শিল্পকলা", obtained: 48 }
+            { subject: "বাংলা", obtained: 64 }, { subject: "ইংরেজি", obtained: 73 },
+            { subject: "প্রাথমিক গণিত", obtained: 33 }, { subject: "বাংলাদেশ ও বিশ্ব পরিচয়", obtained: 52 },
+            { subject: "প্রাথমিক বিজ্ঞান", obtained: 51 }, { subject: "ধর্ম ও নৈতিক শিক্ষা", obtained: 76 },
+            { subject: "শারীরিক-মানসিক স্বাস্থ্য", obtained: 48 }, { subject: "সঙ্গীত ও শিল্পকলা", obtained: 48 }
         ]
     }
-    // রোল ০৩ থেকে ১৮ পর্যন্ত ডাটা আপনার আগের দেওয়া তালিকা অনুযায়ী যুক্ত হবে।
 ];
